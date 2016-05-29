68084a19de8d8c3e415fd8ac7d59c7cfd9a93fb8
galaktus30 <obuntu@wp.pl>
2016-05-29 14:19:20 +0200

    1

--- desktop/src/pl/javadevmatt/tutorialclicker/desktop/DesktopLauncher.java
+++ desktop/src/pl/javadevmatt/tutorialclicker/desktop/DesktopLauncher.java
@@ -1,18 +1,18 @@
 package pl.javadevmatt.tutorialclicker.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import pl.javadevmatt.tutorialclicker.TutorialClickerGame;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		
 		config.title = TutorialClickerGame.GAME_NAME;
 		config.width = TutorialClickerGame.WIDTH;
 		config.height = TutorialClickerGame.HEIGHT;
 		config.resizable = false;
-		
+
 		new LwjglApplication(new TutorialClickerGame(), config);
 	}
 }
