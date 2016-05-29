1434660bdc49249be4bb632f4d336fff9006f810
galaktus30 <obuntu@wp.pl>
2016-05-29 14:20:02 +0200

    v

--- core/src/pl/javadevmatt/tutorialclicker/TutorialClickerGame.java
+++ core/src/pl/javadevmatt/tutorialclicker/TutorialClickerGame.java
@@ -5,22 +5,21 @@ import pl.javadevmatt.tutorialclicker.screens.SplashScreen;
 import com.badlogic.gdx.Game;
 
 public class TutorialClickerGame extends Game {
-	
+
 	public final static String GAME_NAME = "Tutorial Clicker";
-	
+
 	public final static int WIDTH = 480;
 	public final static int HEIGHT = 700;
-	
+	// gyyy
 	private boolean paused;
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		this.setScreen(new SplashScreen(this));
 	}
-	
+
 	/**
-	 * ---------------------
-	 * getters and setters
+	 * --------------------- getters and setters
 	 * 
 	 */
 
